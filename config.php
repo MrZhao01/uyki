40220166c487a4a6c6bba63f4f5971a29e0fe45b
MrZhao01 <m15992167959@163.com>
2022-11-19 14:40:22 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'demo',
+    'username' => 'demo',
+    'password' => 'a1597258302',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://43.139.77.45',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
